31eaec88e04a48bc730fcc58c3cfdc2d778eadea
ejbeckett <ethel.beckett@griffithuni.edu.au>
2020-07-23 17:31:56 +1000

    merge conflict resolved

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+HEAD
 console.log("I caused a merged conflict");
-=======
+
 function addNum(a, b) {
     return a + b;
 
 }
 console.log("Hello World");
 console.log(5 + 10);
->>>>>>> addingfunction
+addingfunction
